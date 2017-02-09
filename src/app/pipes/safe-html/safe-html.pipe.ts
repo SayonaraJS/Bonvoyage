026156bdcb6dfba039b9be1542989695a4ff9f1b
Aaron Turner <aaron@aaronthedev.com>
2017-02-09 14:55:15 -0800

    Changed the site colors, and removed some todos

--- src/app/pipes/safe-html/safe-html.pipe.ts
+++ src/app/pipes/safe-html/safe-html.pipe.ts
@@ -5,7 +5,7 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser'
   name: 'safeHtml'
 })
 export class SafeHtmlPipe implements PipeTransform {
-
+    // Filter to not remove style tags: http://stackoverflow.com/questions/39628007/angular2-innerhtml-binding-remove-style-attribute
     constructor(private sanitized: DomSanitizer) {}
     transform(value: string) : SafeHtml {
         return this.sanitized.bypassSecurityTrustHtml(value);
